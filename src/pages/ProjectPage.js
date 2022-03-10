dca52820202e73aa043a8f5edef54263d78b5101
Agustin Gramajo <agustingramajo@gmail.com>
2022-03-10 19:19:55 +0100

    Linked to sanity

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -30,48 +30,34 @@ const SectionStyles = styled.section`
 `;
 
 function ProjectPage() {
-  let project = useParams();
-  console.log(project);
+  let projectSlug = useParams();
   const context = useContext(PortfolioContext);
+
   if (context.loading) {
     return <p>Loading...</p>;
-  } else {
-    return (
-      <SectionStyles>
-        {/* <div>{console.log(context.projects.general)}</div> */}
-        <div>
-          <h2>Example Title</h2>
-          <h3>Location: San Miguel de Tucuman</h3>
-          <p>
-            Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse efficitur rutrum
-            tellus in molestie. Morbi vulputate ultricies odio, sed luctus augue efficitur sit amet. Aliquam
-            non dictum risus. Vestibulum faucibus, magna id vulputate pulvinar, neque nisl viverra est, id
-            consectetur nisi dui a enim. Maecenas maximus congue sodales. In sit amet arcu ex. Suspendisse
-            luctus odio quis pellentesque maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi dictum nulla eros, eu laoreet quam vulputate sit amet. Nam sagittis purus et vehicula
-            efficitur. Vivamus blandit est vitae convallis varius. Duis vel placerat ante. Morbi ultricies
-            orci sit amet sem hendrerit dictum. Sed vitae nunc sollicitudin, tempus libero vitae, aliquam
-            libero. Phasellus placerat justo et odio vehicula suscipit. Sed vulputate lacinia lacus, vel
-            venenatis ante maximus volutpat. In hac habitasse platea dictumst. Nam et dui sit amet purus
-            fermentum imperdiet. Fusce tincidunt nibh at lorem rutrum, ac congue felis pellentesque. Quisque
-            vitae tincidunt arcu, non iaculis sapien. Integer varius, sem sit amet feugiat vestibulum, elit
-            urna suscipit ex, a tempus velit nisl in est. Nulla scelerisque, elit viverra venenatis accumsan,
-            ante est semper magna, ut imperdiet eros nibh sit amet turpis. In ipsum eros, posuere eget enim
-            at, consectetur tincidunt turpis. Curabitur pulvinar placerat volutpat. Praesent nec nisi nec
-            risus maximus commodo. Duis pharetra non libero ac convallis. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum malesuada vehicula ante, sit
-            amet tincidunt ligula efficitur tincidunt. Sed sagittis porta neque, a pellentesque erat
-            sollicitudin vitae. Proin accumsan pellentesque aliquam. Etiam ut erat eros. Integer quis mauris
-            non eros lacinia porta vitae a nibh. Duis tincidunt diam a tellus eleifend, vitae tincidunt sapien
-            eleifend. Ut et lorem at leo egestas egestas. Sed sagittis tincidunt libero, eget posuere est. Sed
-            faucibus dolor vitae magna pharetra egestas. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Nullam in felis euismod, imperdiet dui placerat, hendrerit odio. Pellentesque habitant morbi
-            tristique senectus et netus et malesuada fames ac turpis egestas. Nunc sed dolor placerat, varius
-            orci id, auctor libero.
-          </p>
-        </div>
-      </SectionStyles>
-    );
+  } else if (!context.loading) {
+    for (let individualProject of context.projects.general) {
+      if (projectSlug.project === individualProject.slug.current) {
+        return (
+          <SectionStyles>
+            <div>
+              {individualProject.projectImages.map((image) => (
+                <img src={image.asset.url} alt="Project" />
+              ))}
+            </div>
+            <div>
+              <h2>{individualProject.name}</h2>
+              <h3>Location: {individualProject.location}</h3>
+              {individualProject.projectDescription.map((description) => (
+                <p>{description}</p>
+              ))}
+            </div>
+          </SectionStyles>
+        );
+      } else {
+        return <p>Sorry! This project doesn't exists!</p>;
+      }
+    }
   }
 }
 
